073417566cac996e2cc0fae3763d2ee7b3f05409
Konstantinos Pascal <pascalkonstantinos@gmail.com>
2020-06-25 06:04:28 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+test = 'Hello'
+
+print(test + ' there!')
+print(f'{test} world!')
+
+num1 = 1
+num2 = 2
+
+num3 = num1 + num2
+
+print(num3)
+
+def function1(num1, num2):
+    return print(num1 * num2)
+
+def function2():
+    return print('function2 print')
+
+function1(2, 2)
+function2()
